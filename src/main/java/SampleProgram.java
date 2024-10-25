e3ac419f1e1adc172bbac9adbab15c7cd0985374
Vishwanatha <vthebbar@gmail.com>
2024-10-25 20:55:56 +0800

    Update SampleProgram.java

--- src/main/java/SampleProgram.java
+++ src/main/java/SampleProgram.java
@@ -2,11 +2,11 @@ public class SampleProgram {
 
     public static void main(String[]  args){
 
-        int a=10;
+        int a=20;
         int b=20;
-        int sum=a+b;
+        int sum1=a+b;
 
-        System.out.println("Sum="+ sum);
+        System.out.println("Sum="+ sum1);
 
     }
 }
